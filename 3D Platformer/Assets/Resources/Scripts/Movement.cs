40d7497f9191052b9ce10dd53aca758548482929
JacksonDonaldson <66980702+JacksonDonaldson@users.noreply.github.com>
2020-06-16 20:45:47 -0400

    Add very basic movement to player

---
+++ 3D Platformer/Assets/Resources/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        rb.AddForce(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
