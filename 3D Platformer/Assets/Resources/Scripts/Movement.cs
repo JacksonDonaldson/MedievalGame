bcf2ea0352d1b7362b257792aa741c411a72c163
JacksonDonaldson <66980702+JacksonDonaldson@users.noreply.github.com>
2020-06-18 12:32:17 -0400

    Adds vertical camera movement

--- 3D Platformer/Assets/Resources/Scripts/Movement.cs
+++ 3D Platformer/Assets/Resources/Scripts/Movement.cs
@@ -23,30 +23,47 @@ public class Movement : MonoBehaviour
     void Update()
     {
         
-        //set camera position based on where the player is
-        //todo(2): enable the player to rotate the camera around the player through mouse movement (probably quite difficult)
-
-        //camT.RotateAround(pT.position, Vector3.up, 50 * Time.deltaTime);
-        //todo(1): rotate the camera such that it is always looking at the player (done)
+        
+        //enable the player to rotate the camera around the player through mouse movement
+        //moves the camera around the player at a set distance
+        //gets how far to move from mouse movements
         float horizontal = Input.GetAxis("Mouse X") * camSpeed * Time.deltaTime;
         float vertical = Input.GetAxis("Mouse Y") * camSpeed * Time.deltaTime;
+
+        //first, calculates how far to move vertically, and then figures out how far the camera should be from the player at that height(that's horizontalDistance)
+        float y = camT.localPosition.y - vertical;
+        if (y > distance-0.01d)
+        {
+            y = (float)distance-0.01f;
+        }
+        if (y < 0)
+        {
+            y = 0f;
+        }
+        float horizontalDistance = (float)Math.Pow((Math.Pow(distance, 2d) - Math.Pow((double)y, 2)), 0.5d);
         
+        //find out how far in the x direction to move
         float x = camT.localPosition.x - horizontal;
 
-        if (x > 4)
+        if (x > horizontalDistance)
         {
-            x = 4;
+            x = horizontalDistance;
         }
-        if (x < -4)
+        if (x < -horizontalDistance)
         {
-            x = -4;
+            x = -horizontalDistance;
         }
-        
-        float z = (float)Math.Pow((Math.Pow(distance, 2d) - Math.Pow((double)x, 2)), 0.5d); 
-        camT.localPosition = new Vector3(x, 0, z);
+        //set z acording to how far x moved, and how far horizontalDistance is
+        float z = (float)Math.Pow((Math.Pow(horizontalDistance, 2d) - Math.Pow((double)x, 2)), 0.5d); 
+        //set new position
+        camT.localPosition = new Vector3(x, y, z);
+
+        //rotate the camera such that it is always looking at the player (done)
         camT.LookAt(pT.position);
 
         //todo(3): better movement
+        //find which direction is forward
+        //pT.eulerAngles = new Vector3(pT.eulerAngles.x, camT.eulerAngles.y, pT.eulerAngles.z);
         //move the player around
         rb.AddForce(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
         rb.AddForce(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
